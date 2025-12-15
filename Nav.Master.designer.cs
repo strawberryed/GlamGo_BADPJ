c02422c1ccdeb2655665c6710c322f7f34dffdb4
Abby <abbychia12@gmail.com>
2025-12-15 10:56:33 +0800

    go back

--- Nav.Master.designer.cs
+++ Nav.Master.designer.cs
@@ -33,12 +33,12 @@ namespace GlamGo_BADPJ
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// MainContent control.
+        /// navBar control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder navBar;
     }
 }
